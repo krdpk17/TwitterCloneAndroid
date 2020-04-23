fa14710fe47bac1c8e03c88848e06ad125d50acd
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-04-23 14:33:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.twitterclone
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.multidex.MultiDexApplication
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun onLogin(v: View){
+
+    }
+
+    fun gotoSignup(v: View){
+
+    }
+}
