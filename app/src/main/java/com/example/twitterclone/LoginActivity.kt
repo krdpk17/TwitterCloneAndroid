7b4e91e8d332fe3a59c22689ce2ae5afac087068
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-04-23 15:26:11 +0530

    Support of login check using google firebase

--- app/src/main/java/com/example/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -1,21 +1,91 @@
 package com.example.twitterclone
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.multidex.MultiDexApplication
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser?.uid
+        user?.let{
+            startActivity(HomeActivity.newIntent(this))
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setTextChangeListener(emailET, emailITIL)
+        setTextChangeListener(passwordET, passwordITIL)
+        loginProgressLayout.setOnTouchListener { v, event ->  true}
     }
-    fun onLogin(v: View){
 
+    fun setTextChangeListener(et:EditText, til:TextInputLayout){
+        et.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                til.isErrorEnabled = false
+            }
+        })
+    }
+
+    fun onLogin(v: View){
+        var proceed = true
+        if(emailET.text.isNullOrEmpty()){
+            emailITIL.error = "Email is required"
+            emailITIL.isErrorEnabled = true
+            proceed = false
+        }
+        if(passwordET.text.isNullOrEmpty()){
+            passwordITIL.error = "Password is required"
+            passwordITIL.isErrorEnabled = true
+            proceed = false
+        }
+
+        if(proceed){
+            loginProgressLayout.visibility = View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(emailET.text.toString(), passwordET.text.toString())
+                .addOnCompleteListener{ task->
+                    if(!task.isSuccessful){
+                        loginProgressLayout.visibility = View.GONE
+                        Toast.makeText(this@LoginActivity, "Login Error: ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener{ e:Exception->
+                    e.printStackTrace()
+                    loginProgressLayout.visibility = View.GONE
+                }
+
+        }
     }
 
     fun gotoSignup(v: View){
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener( firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 }
