676fe69773d84114b842d758a154afce07bfbf29
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-04-23 17:07:28 +0530

    Support of Signup a new user using google firebase

---
+++ app/src/main/java/com/example/twitterclone/util/Model.kt
@@ -0,0 +1,10 @@
+package com.example.twitterclone.util
+
+data class User(
+    val email:String? = "",
+    val username:String? = "",
+    val imageUrl:String? = "",
+    val followHashtags:ArrayList<String>? = arrayListOf(),
+    val followUsers:ArrayList<String>? = arrayListOf()
+
+)
